49dda56910898bfe55db143482f5cfd25ad053a6
Victor Reipur <70368750+vicre@users.noreply.github.com>
2025-11-24 14:52:05 +0100

    Adjust HIBP routes to mirror upstream API paths

--- backend/app-main/hibp/urls.py
+++ backend/app-main/hibp/urls.py
@@ -3,21 +3,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hibp/v3/subscribeddomains", views.SubscriptionDomainsView.as_view()),
-    path("hibp/v3/subscription/status", views.SubscriptionStatusView.as_view()),
-    path("hibp/v3/breacheddomain/<str:domain>", views.BreachedDomainView.as_view()),
-    path("hibp/v3/dataclasses", views.DataClassesView.as_view()),
-    path("hibp/v3/breachedaccount/<str:account>", views.BreachedAccountView.as_view()),
-    path("hibp/v3/breaches", views.AllBreachesView.as_view()),
-    path("hibp/v3/breach/<str:name>", views.SingleBreachView.as_view()),
-    path("hibp/v3/pasteaccount/<str:account>", views.PasteAccountView.as_view()),
+    path("hibp/api/v3/subscribeddomains", views.SubscriptionDomainsView.as_view()),
+    path("hibp/api/v3/subscription/status", views.SubscriptionStatusView.as_view()),
+    path("hibp/api/v3/breacheddomain/<str:domain>", views.BreachedDomainView.as_view()),
+    path("hibp/api/v3/dataclasses", views.DataClassesView.as_view()),
+    path("hibp/api/v3/breachedaccount/<str:account>", views.BreachedAccountView.as_view()),
+    path("hibp/api/v3/breaches", views.AllBreachesView.as_view()),
+    path("hibp/api/v3/breach/<str:name>", views.SingleBreachView.as_view()),
+    path("hibp/api/v3/pasteaccount/<str:account>", views.PasteAccountView.as_view()),
     path(
-        "hibp/v3/stealerlogsbyemaildomain/<str:domain>",
+        "hibp/api/v3/stealerlogsbyemaildomain/<str:domain>",
         views.StealerLogsByEmailDomainView.as_view(),
     ),
-    path("hibp/v3/stealerlogsbyemail/<str:account>", views.StealerLogsByEmailView.as_view()),
     path(
-        "hibp/v3/stealerlogsbywebsitedomain/<str:domain>",
+        "hibp/api/v3/stealerlogsbyemail/<str:account>",
+        views.StealerLogsByEmailView.as_view(),
+    ),
+    path(
+        "hibp/api/v3/stealerlogsbywebsitedomain/<str:domain>",
         views.StealerLogsByWebsiteDomainView.as_view(),
     ),
     path("hibp/range/<str:prefix>", views.PwnedPasswordsRangeView.as_view()),
